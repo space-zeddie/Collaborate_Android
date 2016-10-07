697ba988fc911bd417eb8d8c16db4aad52ecdcbe
space-zeddie <thespacezeddie@gmail.com>
2016-10-08 01:19:53 +0300

    addeed notification settings

---
+++ app/src/main/java/com/example/citizenzer0/collaborate_mhw/NotificationSettings.java
@@ -0,0 +1,11 @@
+package com.example.citizenzer0.collaborate_mhw;
+
+/**
+ * Created by citizenzer0 on 10/8/16.
+ */
+
+public class NotificationSettings {
+    public static String SenderId = "704416806825";
+    public static String HubName = "test2kutsyk";
+    public static String HubListenConnectionString = "Endpoint=sb://test2kutsyk.servicebus.windows.net/;SharedAccessKeyName=DefaultListenSharedAccessSignature;SharedAccessKey=KvPVn+rx1DmOEZrHtLM6eEOpcqdlciU4EgndklIvgLs=";
+}
