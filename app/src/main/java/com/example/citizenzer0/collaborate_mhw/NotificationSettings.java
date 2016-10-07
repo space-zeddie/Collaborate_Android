49bb0c203191a85f429bd69fc7452b59fe7ce9b7
space-zeddie <thespacezeddie@gmail.com>
2016-10-08 02:45:12 +0300

    enabled push notifications from the app

--- app/src/main/java/com/example/citizenzer0/collaborate_mhw/NotificationSettings.java
+++ app/src/main/java/com/example/citizenzer0/collaborate_mhw/NotificationSettings.java
@@ -7,5 +7,6 @@ package com.example.citizenzer0.collaborate_mhw;
 public class NotificationSettings {
     public static String SenderId = "704416806825";
     public static String HubName = "test2kutsyk";
+    public static String HubFullAccess = "Endpoint=sb://test2kutsyk.servicebus.windows.net/;SharedAccessKeyName=DefaultFullSharedAccessSignature;SharedAccessKey=eYI8JSrXeKffD5K34xP2+t7Q/ru8JSeXXWEuFyYX5Jw=";
     public static String HubListenConnectionString = "Endpoint=sb://test2kutsyk.servicebus.windows.net/;SharedAccessKeyName=DefaultListenSharedAccessSignature;SharedAccessKey=KvPVn+rx1DmOEZrHtLM6eEOpcqdlciU4EgndklIvgLs=";
 }
