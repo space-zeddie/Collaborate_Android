e96bc11664a68f1be3dfd899f545b421f307f168
space-zeddie <thespacezeddie@gmail.com>
2016-10-07 20:50:52 +0300

    created basic android project with login activity

---
+++ app/src/main/java/com/example/citizenzer0/collaborate_mhw/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.citizenzer0.collaborate_mhw;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.app.NotificationManager;
+import android.app.Notification;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+       // NotificationManager manager = new
+    }
+}
